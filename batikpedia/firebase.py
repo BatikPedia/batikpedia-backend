713eb1bcae3a882302a56a6db93b00a0980010c9
darielgaizta <darielgaz@gmail.com>
2023-12-17 21:19:51 +0700

    configure firebase from env

--- batikpedia/firebase.py
+++ batikpedia/firebase.py
@@ -1,28 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from dotenv import dotenv_values
 import json
+import os
 
-# Write some neccessary constants here
-SERVICE_ACCOUNT_KEY = 'service_account_key'
+FIREBASE_SERVICE_ACCOUNT = os.getenv('FIREBASE_SERVICE_ACCOUNT', None)
+if not FIREBASE_SERVICE_ACCOUNT: raise Exception('Firebase service account is not configure.')
 
-env = dotenv_values('.env')
-
-firebase, data = {
-    SERVICE_ACCOUNT_KEY: {k: v for k, v in env.items() if k.startswith('FIREBASE.SERVICE_ACCOUNT_KEY')}
-}, {}
-
-for key in firebase.keys():
-    payload = ''
-    for k, v in firebase[key].items():
-        k = k.lower()
-        payload += f'  "{k[k.index(key) + len(key) + 1:]}": "{v}",\n'
-    payload = '{\n' + payload[:-2] + '\n}'
-    data.update({key: json.loads(payload)})
-
-# Certificate definition
-cred = credentials.Certificate(data.get(SERVICE_ACCOUNT_KEY, None))
+cred = credentials.Certificate(json.loads(FIREBASE_SERVICE_ACCOUNT))
 
 firebase_admin.initialize_app(cred)
 
