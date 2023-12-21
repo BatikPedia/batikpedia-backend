87bb12e30a0826c503c5bd54a55f2590b8ff25ea
Robertus Aditya Sukoco <raditskc@gmail.com>
2023-12-22 02:57:07 +0700

    just a few extra steps, nothing too fancy

--- api/apps.py
+++ api/apps.py
@@ -2,12 +2,9 @@ from django.apps import AppConfig
 from django.conf import settings
 from tensorflow import keras
 import os
-from google.cloud import storage
+from tensorflow.python.lib.io import file_io
 
 BUCKET_NAME = "batikpedia-apigateway-bucket-staging"
-
-storage_client = storage.Client()
-bucket = storage_client.bucket(BUCKET_NAME)
 
 
 class ApiConfig(AppConfig):
@@ -16,5 +13,14 @@ class ApiConfig(AppConfig):
 
 class BatikPredictionModelConfig(AppConfig):
     name = 'predictionAPI'
-    model_file = bucket.blob('ML-model/batik_model_final.h5')
-    model = keras.models.load_model(model_file.name)
+    model_file = file_io.FileIO('gs://{}/ML-model/batik_model_final.h5'.format(BUCKET_NAME), mode='rb')
+
+    # Save the model file to a local temporary file
+    temp_local_model_file = '/tmp/batik_model_temp.h5'
+    with open(temp_local_model_file, 'wb') as temp_file:
+        temp_file.write(model_file.read())
+
+    # Load the model using keras.models.load_model
+    loaded_model = keras.models.load_model(temp_local_model_file)
+
+    model = keras.models.load_model(temp_local_model_file)
